611dafeb5e2752916cb57de09c82fae46c421322
Geoffrey NEGRI <geof@Geoffreys-MacBook-Air.home>
2021-10-23 00:26:01 +0200

    Added first lesson - creating a 3D scene with camera and cube

---
+++ 01-basic_scene/script.js
@@ -0,0 +1,32 @@
+const sizes = {
+    width: 800,
+    height: 600
+};
+
+// Create scene
+const scene = new THREE.Scene();
+
+// Create red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Camera (2 parameters : FieldOfView, AspectRatio)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.x = 1;
+camera.position.y = 1;
+
+console.log(camera.position);
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector('canvas.webgl');
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
